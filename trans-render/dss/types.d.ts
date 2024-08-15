bcb4a0a4deaf2a8b16faba759e48a47486998c49
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-08-15 18:57:33 -0400

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

--- trans-render/dss/types.d.ts
+++ trans-render/dss/types.d.ts
@@ -120,7 +120,13 @@ export type CSSSelector = string;
  */
 export type MarkerString = string;
 
-export interface GetPHOptions{
+/**
+ * PIP stands for Partner in Prop (for now) -- supports bi-directional data flow to property
+ * IP stands for In Prop (for now) -- Data only Flow only goes in
+ * OP s
+ */
+
+export interface GetPIPOptions{
     //name of event to listen for for when the prop being monitored for changes
     evtName?: string,
     isRoundAboutReady?: boolean;

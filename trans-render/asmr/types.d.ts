18f04b04757928e63c19c55ccdde91447e05c4ca
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-08-22 18:40:03 -0400

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

--- trans-render/asmr/types.d.ts
+++ trans-render/asmr/types.d.ts
@@ -47,6 +47,10 @@ export interface ASMROptions<TProp = any>{
     falseText?: FalseText;
     trueValue?: TrueStringValue;
     falseValue?: FalseStringValue;
+    /**
+     * Mind Reading ID
+     */
+    mrID?: string;
 }
 
 export interface SetOptions<TProp = any> extends ASMROptions<TProp>{

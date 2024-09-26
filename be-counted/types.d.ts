5ec15232eee0680eed36633c4e639ecf9dbdd2e9
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-09-26 06:18:18 -0400

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

--- be-counted/types.d.ts
+++ be-counted/types.d.ts
@@ -21,16 +21,7 @@ export interface EndUserPropsBasic{
      * Make count loop back to min value after hitting the ceiling set by ltOrEq or lt
      */
     loop?: boolean;
-    /**
-     * DTR transform to perform after count increments
-     * See https://github.com/bahrus/trans-render for syntax
-     */
-    xform?: XForm<EndUserPropsBasic, Actions, {}>;
-    /**
-     * Specify how wide the surrounding DOM should be subject to the transformation.
-     * Values specified here: https://github.com/bahrus/trans-render/blob/baseline/lib/types.d.ts#L388
-     */
-    transformScope?: Scope;
+
     /**
      * Slowly "awaken" a disabled element.  If the disabled attribute is not set to a number, or is set to "1", removes the disabled attribute.  If it is a larger number, decrements the number by 1. 
      */
@@ -64,15 +55,13 @@ export type PAP = Partial<AP>;
 
 export type ProPAP = Promise<PAP>
 
-export type POA = [PAP | undefined, ActionOnEventConfigs<PAP, Actions>];
-
-export type ProPOA = Promise<POA>
+export type BAP = AllProps & BEAllProps;
 
  export interface Actions{
-    hydrate(self: this, mold: PAP): ProPOA;
-    inc(self: this): PAP;
-    disableInc(self: this): POA;
-    check(self: this, allGood: PAP): PAP;
-    hydrateTransform(self: this): ProPAP;
+    hydrate(self: this, mold: PAP): ProPAP;
+    // inc(self: this): PAP;
+    // disableInc(self: this): POA;
+    // check(self: this, allGood: PAP): PAP;
+    // hydrateTransform(self: this): ProPAP;
 }
 

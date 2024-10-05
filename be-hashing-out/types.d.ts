66c4d6da57af6017121656a7e7966586d954689a
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-10-05 08:42:14 -0400

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

---
+++ be-hashing-out/types.d.ts
@@ -0,0 +1,9 @@
+import {BEAllProps, IEnhancement} from '../trans-render/be/types.d.ts';
+
+export interface EndUserProps extends IEnhancement{
+    hash: string;
+}
+
+export interface AllProps extends EndUserProps {
+
+}

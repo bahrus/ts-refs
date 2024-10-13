1d93320c96e45446e434e516e10c5f3b14722670
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-10-13 13:04:17 -0400

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

---
+++ be-alit/types.d.ts
@@ -0,0 +1,27 @@
+import {BEAllProps, IEnhancement} from '../trans-render/be/types';
+
+export interface EndUserProps extends IEnhancement{
+    eval?: string,
+    vm?: any,
+    scriptRef?: string,
+}
+
+export interface AP extends EndUserProps{
+    attrExpr?: string | null,
+    scriptEl?: HTMLScriptElement,
+    renderer?: (vm: any, enhancedElement: Element) => any,
+}
+
+export type PAP = Partial<AP>;
+
+export type BAP = AP & BEAllProps;
+
+export type ProPAP = Promise<PAP>;
+
+export interface Actions {
+    // getAttrExpr(self: this): PAP;
+    // onAttrExpr(self: this): PAP;
+    // importSymbols(self: this): ProPAP;
+    // doRender(self: this): void;
+    // onScriptRef(self: this): ProPAP;
+}

9db9fbe7488e0dcb98a2d7b2a597be153d632c29
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-10-23 22:50:37 -0400

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

--- be-reformable/types.d.ts
+++ be-reformable/types.d.ts
@@ -4,10 +4,11 @@ export interface EndUserProps extends IEnhancement<HTMLFormElement>{
     baseLink?: string,
     path?: string,
     target?: '-object'
+    updateOn: string,
 }
 
 export interface AllProps extends EndUserProps{
-
+    updateCnt: number,
 }
 
 export type AP = AllProps;
@@ -19,5 +20,5 @@ export type ProPAP = Promise<PAP>;
 export type BAP = AP & BEAllProps;
 
 export interface Actions {
-    
+    hydrate(self: BAP): ProPAP;
 }

cd9a52bfeae9c31ed4f0e69833706b14205190f3
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-06 10:22:52 -0500

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

--- be-alit/types.d.ts
+++ be-alit/types.d.ts
@@ -1,4 +1,5 @@
 import {BEAllProps, IEnhancement} from '../trans-render/be/types';
+import {AbsorbingObject} from '../trans-render/asmr/types';
 
 export interface RenderingHTMLScriptElement extends HTMLScriptElement{
     renderer: (vm: any, html: any) => any,
@@ -6,11 +7,12 @@ export interface RenderingHTMLScriptElement extends HTMLScriptElement{
 
 export interface EndUserProps extends IEnhancement<RenderingHTMLScriptElement>{
     vm: any,
-    with: string,
+    with: Array<string>,
 }
 
 export interface AP extends EndUserProps{
     renderer: (vm: any, html: any) => any,
+    absorbingObject: AbsorbingObject
 }
 
 export type PAP = Partial<AP>;
@@ -22,5 +24,5 @@ export type ProPAP = Promise<PAP>;
 export interface Actions {
     getRenderer(self: BAP): BAP;
     doRender(self: BAP): void;
-    
+    observe(self: BAP): ProPAP;
 }

ba2fc9bf53c85c8a9f8426ab9cb8e914456db8b1
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-05 21:32:36 -0500

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

---
+++ be-eventing/types.d.ts
@@ -0,0 +1,21 @@
+import {IEnhancement, BEAllProps} from '../trans-render/be/types';
+
+export interface EventHandlingScriptElement extends HTMLScriptElement{
+    on: any
+}
+
+export interface EndUserProps extends IEnhancement<EventHandlingScriptElement>{
+
+}
+
+export interface AllProps extends EndUserProps{}
+
+export type AP = AllProps;
+
+export type PAP = Partial<AP>;
+
+export type ProPAP = Promise<PAP>;
+
+export type BAP = AP & BEAllProps;
+
+export interface Actions{}

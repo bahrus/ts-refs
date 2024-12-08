da27d47ef4971933aa7fb4bcb4fefa1f1e3ff333
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-07 20:03:38 -0500

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

---
+++ be-written/types.d.ts
@@ -0,0 +1,36 @@
+import {IEnhancement, BEAllProps} from '../trans-render/be/types';
+import {EndUserProps as BeBasedEndUserProps} from '../be-based/types';
+import {Inserts} from '../../node_modules/stream-orator/types';
+
+export interface EndUserProps extends IEnhancement{
+    from?: string,
+    to?: string,
+    shadowRootMode?: 'open' | 'closed'
+    reqInit?: RequestInit,
+    wrapper?: string,
+    beBased?: boolean | BeBasedEndUserProps,
+    beOosoom?: string,
+    defer?: boolean,
+    inProgressCss?: string,
+    inserts?:Inserts,
+    between?: [lhs: string, rhs: string],
+    once?: boolean,
+}
+
+export interface AllProps extends EndUserProps{
+    to: string,
+    from: string,
+}
+
+export interface AllProps extends EndUserProps {}
+
+export type AP = AllProps;
+
+export type PAP = Partial<AP>;
+
+export type ProPAP = Promise<PAP>;
+
+
+export interface Actions{
+    write(self: AP & BEAllProps): ProPAP;
+}

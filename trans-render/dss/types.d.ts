58780fe05f36cfaab783da0240d4297293b38d41
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-10 22:59:18 -0500

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

--- trans-render/dss/types.d.ts
+++ trans-render/dss/types.d.ts
@@ -41,6 +41,15 @@ export type ElementSigils = '/' | '~';
 
 export type Sigils = AttrSigils | ElementSigils;
 
+export type asOptions = 
+    | 'number'
+    | 'boolean'
+    | 'string' 
+    | 'object'
+    | 'regexp' 
+    | 'urlpattern'
+;
+
 export interface Specifier {
     /** Directional Scope Sigil */
     dss?: DirectionalScopeSigils,
@@ -84,20 +93,8 @@ export interface Specifier {
      */
     hpf?: string;
     
-    as?: 
-        | 'Number' 
-        | 'number'
-        | 'Boolean'
-        | 'boolean'
-        | 'String'
-        | 'string' 
-        | 'Object'
-        | 'object'
-        | 'RegExp'
-        | 'regexp' 
-        | 'URLPattern'
-        | 'urlpattern'
-    ;
+    as?: asOptions
+        
     
     isModulo?: boolean;
     modulo?: Modulo;

32bff75a104d76b8758d2707eb3c0e12f53a3029
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-15 18:48:09 -0500

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

--- be-observing/types.d.ts
+++ be-observing/types.d.ts
@@ -47,6 +47,7 @@ export interface ObservingParameters{
     mappings?: Array<AndIfThen>,
     aggKey: StringWithAutocompleteOptions<aggKeys>,
     JSExpr: string,
+    ONExpr: string,
     //aggregateRemoteVals?: 'Union' | 'Conjunction' | 'ObjectAssign' | 'Sum' | 'Product' | 'ArrayPush'
 }
 

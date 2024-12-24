c28b696d726465f3792fa3e00aa005d96607224a
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-24 18:03:41 -0500

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

---
+++ be-elevating/types.d.ts
@@ -0,0 +1,34 @@
+import {IEnhancement, BEAllProps} from '../trans-render/be/types';
+
+
+export interface EndUserProps extends IEnhancement{
+    /**
+     * abbrev for pass server-rendered value
+     * This will pass the initial value from the enhanced element if applicable
+     */
+    passSRV?: boolean
+}
+
+export interface AP extends EndUserProps {
+    parsedStatements?: Array<ElevatingParameters>,
+    rawStatements?: Array<string>,
+}
+
+export type AllProps = AP;
+
+export type PAP = Partial<AP>
+
+export type ProPAP  = Promise<PAP>
+
+export interface Actions{
+    noAttrs(self: this): ProPAP;
+    hydrate(self: this): ProPAP;
+    onRawStatements(self: this): void;
+}
+
+export interface ElevatingParameters {
+    localPropToElevate?: string,
+    remoteSpecifiers: Array<Specifier>,
+    localEventType?: string,
+}
+

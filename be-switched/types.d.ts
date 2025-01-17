1f4bc2eca22e89de78b165d25f37227439fe6d87
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-01-17 06:24:30 -0500

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

--- be-switched/types.d.ts
+++ be-switched/types.d.ts
@@ -69,10 +69,10 @@ export interface TwoValueSwitch{
     withinSpecifier?: Specifier,
     req?: boolean,
     op?: Op,
-    negate?: boolean,
+    //negate?: boolean,
     lhs?: ISide,
     rhs?: ISide,
-    onOrOff?: 'on' | 'On' | 'off' | 'Off'
+    onOrOff: 'on' | 'On' | 'off' | 'Off'
 }
 
 export interface Dependency extends Specifier{}
@@ -129,12 +129,12 @@ export interface Elevate {
 
 export interface EventForNValueSwitch {
     ctx: NValueScriptSwitch,
-    factors: {[key: string] : SignalRefType},
+    factors: {[key: string] : any},
     switchOn?: boolean,
     elevate?: Elevate
 }
 
-export interface SignalAndEvent {
-    signal?: WeakRef<SignalRefType>,
-    eventSuggestion?: string
-}
+// export interface SignalAndEvent {
+//     signal?: WeakRef<SignalRefType>,
+//     eventSuggestion?: string
+// }

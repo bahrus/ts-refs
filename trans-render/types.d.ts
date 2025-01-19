459ba50c941c580667633537f7afccfd48c6533c
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-01-19 06:46:35 -0500

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

--- trans-render/types.d.ts
+++ trans-render/types.d.ts
@@ -468,6 +468,12 @@ export interface MntCfg<TProps = any, TActions = TProps, ETProps = TProps> exten
      */
     lcXform?: XForm<TProps, TActions>,
 
+
+    /**
+     * transforms within ShadowRoot if applicable that uses view transitions 
+     */
+    xxform?: XForm<TProps, TActions>
+
     styles?: /*CSSStyleSheet[] |*/ string | string[] | CSSStyleSheet | Array<CSSStyleSheet>;
 
     shadowRootInit?: ShadowRootInit,

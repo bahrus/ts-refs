764eab21ca569e2c25fbe34863e68bba65f890ec
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-02-14 07:05:10 -0500

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

--- css-charts/types.d.ts
+++ css-charts/types.d.ts
@@ -1,9 +1,15 @@
+import { ITransformer } from "../trans-render/types";
+
 export interface EndUserProps {
 
 }
 
-export interface AllProps extends EndUserProps{}
+export interface AllProps extends EndUserProps{
+    slot: HTMLSlotElement,
+    slotChangeCount: number,
+}
 
 export interface Actions {
-    handleSlotChange(evt: Event, self: AllProps): void;
+    
+    handleSlotChange(evt: Event, transformer: ITransformer<AllProps, Actions, any>): void;
 }

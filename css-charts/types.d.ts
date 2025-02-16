f8ae438ccbb5c185ec28c0cc62024600d539c9b5
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-02-16 13:02:51 -0500

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

--- css-charts/types.d.ts
+++ css-charts/types.d.ts
@@ -21,8 +21,6 @@ export type PAP = Partial<AllProps>;
 export type ProPAP = Promise<PAP>;
 
 export interface Actions {
-    //handleSlotChange(evt: Event, transformer: ITransformer<AllProps, Actions, any>): void;
     extractData(self: AllProps): ProPAP;
 
-    buildTable(self: AllProps): PAP;
 }

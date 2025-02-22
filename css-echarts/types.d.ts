1750c8729b28048b5f9288e43cfa65824d9bf3a1
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-02-21 23:40:23 -0500

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

---
+++ css-echarts/types.d.ts
@@ -0,0 +1,8 @@
+import {AllProps as BaseAllProps, EndUserProps as BaseEndUserProps} from '../css-charts/types';
+
+export interface EndUserProps extends BaseEndUserProps {
+}
+
+export interface AllProps extends EndUserProps, BaseAllProps {
+}
+

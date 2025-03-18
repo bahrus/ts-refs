9543de07d07f19fabe15b7a0aacf2f23ee62ab24
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-03-18 08:52:18 -0400

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

---
+++ be-inclusive/types.d.ts
@@ -0,0 +1,26 @@
+import {IEnhancement, BEAllProps} from '../trans-render/be/types';
+import { XForm } from "../trans-render/types";
+
+export interface EndUserProps<TProps, TMethods, TElement = {}> extends IEnhancement<HTMLTemplateElement>{
+    of: string,
+    xform?: XForm<TProps, TMethods, TElement>,
+    initModel?: TProps & TMethods,
+    slotMap?: any,
+}
+
+export interface AllProps<TProps, TMethods, TElement = {}> extends EndUserProps<TProps, TMethods, TElement>{
+    isParsed?: boolean,
+    model?: TProps & TMethods,
+}
+
+export type AP = AllProps<any, any, any>;
+
+export type PAP = Partial<AP>;
+
+export type ProPAP = Promise<PAP>;
+
+
+export interface Actions{
+    onInitModel(self: this): ProPAP;
+    startWeaving(self: this): ProPAP;
+}

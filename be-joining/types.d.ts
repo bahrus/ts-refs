978f0ea7b9c86863c4d95e70bfc8259b2187ef06
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-03-30 12:50:44 -0400

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

--- be-joining/types.d.ts
+++ be-joining/types.d.ts
@@ -1,4 +1,5 @@
 import {IEnhancement, BEAllProps} from '../trans-render/be/types';
+import {AllProps as XPAsAllProps} from '../xp-as/types';
 
 export interface EndUserProps extends IEnhancement{
     xpAsAttr: string;
@@ -17,4 +18,9 @@ export type BAP = AP & BEAllProps;
 
 export interface Actions {
     hydrate(self: BAP): ProPAP;
+}
+
+export interface Factor{
+    NameOfProp: string,
+    xpAs: XPAsAllProps,
 }

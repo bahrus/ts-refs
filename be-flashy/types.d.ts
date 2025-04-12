99b8813f6b8406782fe70950bccdeb192b3312d5
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-04-12 12:11:22 -0400

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

---
+++ be-flashy/types.d.ts
@@ -0,0 +1,19 @@
+import {IEnhancement, BEAllProps} from '../trans-render/be/types';
+
+export interface EndUserProps extends IEnhancement{
+    delay: number,
+}
+
+export interface AllProps extends EndUserProps{}
+
+export type AP = AllProps;
+
+export type PAP = Partial<AP>;
+
+export type ProPAP = Promise<PAP>;
+
+export type BAP = AP & BEAllProps;
+
+export interface Actions{
+    
+}

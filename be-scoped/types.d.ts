f7100e04b2938ccadbfb699fadca5eca3e8d7306
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-04-30 20:29:27 -0400

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

--- be-scoped/types.d.ts
+++ be-scoped/types.d.ts
@@ -4,7 +4,9 @@ export interface EndUserProps extends IEnhancement{
     names: string;
 }
 
-export interface AllProps extends EndUserProps{}
+export interface AllProps extends EndUserProps{
+    parsedNames: string[];
+}
 
 export type AP = AllProps;
 
@@ -15,6 +17,7 @@ export type ProPAP = Promise<PAP>;
 export type BAP = AP & BEAllProps;
 
 export interface Actions{
-    hydrate(self: BAP): PAP;
+    parse(self: BAP): PAP;
+    hydrate(self: BAP): ProPAP;
     retire(self: BAP): void;
 }

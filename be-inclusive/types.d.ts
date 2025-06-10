3f4d2f0267be760de6116cbe0b0e9af7f51c0a19
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-06-10 19:31:40 -0400

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

--- be-inclusive/types.d.ts
+++ be-inclusive/types.d.ts
@@ -1,12 +1,13 @@
 import {IEnhancement, BEAllProps} from '../trans-render/be/types';
-import { XForm } from "../trans-render/types";
+import {Specifier} from '../trans-render/dss/types';
 
-export interface EndUserProps<TProps, TMethods, TElement = {}> extends IEnhancement<HTMLTemplateElement>{
+export interface EndUserProps<TProps, TMethods, TElement = {}> extends IEnhancement{
     
 }
 
 export interface AllProps<TProps, TMethods, TElement = {}> extends EndUserProps<TProps, TMethods, TElement>{
-    
+    includeRules: Array<IncludeRule>;
+    nodesToInclude: Array<Element>;
 }
 
 export type AP = AllProps<any, any, any>;
@@ -19,5 +20,11 @@ export type BAP = AP & BEAllProps;
 
 
 export interface Actions{
-    
+    hydrate(self: BAP): Promise<PAP>;
+    include(self: BAP): void;
+}
+
+export interface IncludeRule {
+    remoteSpecifierString?: string,
+    remoteSpecifier: Specifier,
 }

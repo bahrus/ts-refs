1e9bdf4f9e2c135b85d1cb3ecd4cc55d629984db
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-08-12 07:50:07 -0400

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

--- per-each/types.d.ts
+++ per-each/types.d.ts
@@ -2,21 +2,22 @@ import {IEnhancement, BEAllProps} from '../trans-render/be/types';
 import { HasIshList } from '../trans-render/dss/types';
 
 export interface EndUserProps extends IEnhancement{
-    each: string;
+    //each: string;
     mapIdxTo: string;
     idxStart: number;
     idleTimeout: number;
     options: Partial<EndUserProps>;
+    parsedStatements: Array<LoopingStatement>,
+    rawStatements?: Array<string>,
 }
 
 export interface AllProps extends EndUserProps{
     listProp: string;
     itemProp: string;
-    ish: HasIshList;
-    ishContainer: Element;
-    itemTemplate: HTMLTemplateElement;
+
+    itemTemplates: Array<HTMLTemplateElement>;
     emc: any;
-    //updateCnt: number;
+    loopingParameters: Array<LoopingParameters>,
 }
 
 export type AP = AllProps;
@@ -28,8 +29,19 @@ export type ProPAP = Promise<PAP>;
 export type BAP = AP & BEAllProps;
 
 export interface Actions{
-    parse(self: BAP): PAP;
+    //parse(self: BAP): PAP;
     init(self: BAP): ProPAP;
     hydrate(self: BAP)
+}
+
+export interface LoopingStatement {
+    listProp?: string;
+    itemProp: string;
+}
+
+export interface LoopingParameters extends LoopingStatement {
+    listProp: string;
+    ish: any;
+    ishContainer: Element;
 }
 

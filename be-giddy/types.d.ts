7e313e71c6c5bdcb6327f844ab69635d8fda2669
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-08-30 17:25:32 -0400

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

--- be-giddy/types.d.ts
+++ be-giddy/types.d.ts
@@ -1,6 +1,7 @@
 import {IEnhancement, BEAllProps} from '../trans-render/be/types';
 
 export interface EndUserProps extends IEnhancement{
+    idString: string;
 }
 
 export interface AllProps extends EndUserProps{}

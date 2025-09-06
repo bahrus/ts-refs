0dacec3c7f8ecdf2ac6e9e520d95f9cad4f341cc
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-09-06 06:09:17 -0400

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

--- trans-render/dss/types.d.ts
+++ trans-render/dss/types.d.ts
@@ -1,55 +1,14 @@
 import { Scope } from '../lib/types'
 import { CSSQuery } from '../types';
 
-export type DSS = string;
+export type ID = `#${string}`;
+export type Host = `:host()`;
+export type Hostish = ``;
+export type Target = ID | Host | Hostish;
 
-// export type DirectionalScopeSigils = 
-//     /**
-//      * upward direction, non recursive
-//      */
-//     |'^'  
-//     /**
-//      * downward direction, next element siblings only
-//      */
-//     |'Y' 
-//     /**
-//      * IdRef query
-//      * 
-//      */ 
-//     |'?'
-//     /**
-//      * self
-//      */
-//     |'.'
-//     /**
-//      * modulo
-//      */
-//     |'%'
-//     /**
-//      * itemscoped host
-//      */
-//     |'$'
-//     /**
-//      * comment scope
-//      */
-//     |'/**/'
-//     ;
-
-// export type AttrSigils =
-//     /**
-//      * Reference to self / local element
-//      */
-//     '$0' |
-//     /**
-//      * Reference by ID
-//      */
-//     '#' | '@' |  '-' | '|' | '%';
-
-// export type ElementSigils = '/' | '~';
-
-// export type ScopeSigils = '';
-
-// export type Sigils = AttrSigils | ElementSigils | ScopeSigils;
+export type MindReadProp = ``;
+export type PropPath = `?.${string}`;
+export type ConstVal = `\`${string}\``;
 
 export type asOptions = 
     | 'number'
@@ -60,16 +19,17 @@ export type asOptions =
     | 'urlpattern'
     | 'boolean|number'
 ;
+export type MindReadType = ``;
+export type TypeQualifier = `as ${asOptions}`;
 
-// export interface $copeDetail{
-//     ceName?: string,
-//     itemProp?: string,
-// }
+export type ValExpression = `${PropPath | ConstVal | MindReadProp}${TypeQualifier | MindReadType}`;
 
-// export interface $ScopeHierarchy {
-//     home: Element;
-//     satellites?: Array<Element>;
-// }
+export type MindReadEvent = ``;
+
+export type EvtName = `::${string}`;
+
+export type DSS = `${Target}${ValExpression}${EvtName}`;
+
 
 export interface Specifier {
     id?: string,
@@ -84,9 +44,7 @@ export interface Specifier {
     host?: boolean;
 }
 
-//export type Modulo = 'aria-rowindex' | 'aria-colindex' | 'aria-rowindextext'
 
-export type InferredPropName = string;
 
 /**
  * can contain dot (.) for sub property access and pipes (|) for method invocations
@@ -95,44 +53,3 @@ export type SubPropPath = string;
 
 export type EventName = string;
 
-export type CSSSelector = string;
-
-/**
- * starts with a dash, typically all kebab case 
- * inferred prop name will be camel cased based on this.
- */
-export type MarkerString = string;
-
-/**
- * PIP stands for Partner in Prop (for now) -- supports bi-directional data flow to property
- * IP stands for In Prop (for now) -- Data only Flow only goes in
- * OP s
- */
-
-export interface GetPIPOptions{
-    //name of event to listen for for when the prop being monitored for changes
-    evtName?: string,
-    isRoundAboutReady?: boolean;
-    prop?: string,
-    sota?: string,
-}
-
-/**
- * Partner In Prop
- */
-export interface PIP<TProp = any, TElement = Element> extends EventListenerObject{
-    readonly propagator: EventTarget;
-    async getValue(el: TElement): Promise<TProp | undefined>;
-    async setValue(el: TElement, val: TProp);
-    async hydrate(el: TElement);
-    syncVal(el: TElement);
-    disconnect();
-    toString(nv: TProp): string;
-    readonly outEvtName: string;
-}
-
-
-
-export interface HasIshList {
-    ishList: Array<any>;
-}

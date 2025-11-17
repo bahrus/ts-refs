df81504df3e3bd1254fba8a948eda51a93325e7b
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-11-17 18:29:43 -0500

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

--- be-parsing/types.d.ts
+++ be-parsing/types.d.ts
@@ -1,7 +1,7 @@
 import {IEnhancement, BEAllProps, EMC} from '../trans-render/be/types';
 
 export interface EndUserProps extends IEnhancement{
-    eventName: string
+    nudges: boolean
 }
 
 export interface AllProps extends EndUserProps{
@@ -16,6 +16,7 @@ export type ProPAP = Promise<PAP>;
 export type BAP = AP & BEAllProps;
 
 export interface Actions{
+    do(self: BAP): Promise<void>;
 }
 
 export interface AttrInfo {

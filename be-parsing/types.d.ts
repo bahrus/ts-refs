49da1e82326c131ca94209543670896b456ec494
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-11-23 18:50:36 -0500

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

--- be-parsing/types.d.ts
+++ be-parsing/types.d.ts
@@ -16,7 +16,7 @@ export type ProPAP = Promise<PAP>;
 export type BAP = AP & BEAllProps;
 
 export interface Actions{
-    do(self: BAP): Promise<void>;
+    //do(self: BAP): Promise<void>;
 }
 
 export interface AttrInfo {

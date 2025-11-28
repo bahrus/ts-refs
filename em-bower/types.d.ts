e87f67334f58281ec441e93263cf8aef529c063e
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-11-28 14:39:59 -0500

    Merge branch 'baseline' of https://github.com/bahrus/ts-refs into baseline

--- em-bower/types.d.ts
+++ em-bower/types.d.ts
@@ -1,6 +1,7 @@
 import {IEnhancement, BEAllProps, EMC} from '../trans-render/be/types';
 
 export interface EndUserProps extends IEnhancement{
+    path: string,
 }
 
 export interface AllProps extends EndUserProps{
@@ -15,4 +16,5 @@ export type ProPAP = Promise<PAP>;
 export type BAP = AP & BEAllProps;
 
 export interface Actions{
+    act(self: BAP): PAP
 }
